13047ee144cf1ce0e1d02421379030c7bb843d72
tomori <tmrtkm1227tksx@gmail.com>
2022-06-17 23:23:18 +0900

    :sparkles: ヘッダファイルを追加

---
+++ include/philo.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/17 13:00:29 by ttomori           #+#    #+#             */
+/*   Updated: 2022/06/17 22:54:28 by ttomori          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <limits.h>
+
+void	ft_putstr_fd(char const *s, int fd);
+void	print_usage(void);
+
+#endif
