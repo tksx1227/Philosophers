c5ce8c139cda4a2d4d7e5c56309056674ccd2c3c
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2022-07-02 14:02:07 +0900

    ✨ do_sleepingを追加

--- include/philo.h
+++ include/philo.h
@@ -69,6 +69,7 @@ void		free_philos_circular(t_philo *head);
 void		*main_loop(void *content);
 
 /* Philo Actions */
+int			do_sleeping(t_philo *philo);
 int			do_thinking(t_philo *philo);
 
 #endif
--- src/philo_actions.c
+++ src/philo_actions.c
@@ -12,6 +12,23 @@
 
 #include "philo.h"
 
+int	do_sleeping(t_philo *philo)
+{
+	t_timestamp	now_ms;
+
+	pthread_mutex_lock(&philo->rule->system_status_mutex);
+	if (philo->rule->is_finished)
+	{
+		pthread_mutex_unlock(&philo->rule->system_status_mutex);
+		return (1);
+	}
+	now_ms = get_timestamp_us() / 1000;
+	printf("%lld %d is sleeping\n", now_ms, philo->index);
+	pthread_mutex_unlock(&philo->rule->system_status_mutex);
+	msleep(philo->rule->time_to_sleep);
+	return (0);
+}
+
 int	do_thinking(t_philo *philo)
 {
 	t_timestamp	now_ms;
