4ae26c611575703c6809d9187abf797b201504f2
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2022-07-02 14:15:54 +0900

    ✨ take_two_forksを追加

--- src/philo_actions2.c
+++ src/philo_actions2.c
@@ -14,11 +14,49 @@
 
 static int	return_the_fork(t_philo *philo);
 
+int	take_two_forks(t_philo *philo)
+{
+	int	index;
+
+	index = philo->index;
+	if (take_a_fork(index, philo) || take_a_fork(index, philo->next))
+		return (1);
+	return (0);
+}
+
 int	return_two_forks(t_philo *philo)
 {
 	if (return_the_fork(philo) || return_the_fork(philo->next))
 		return (1);
 	return (0);
+}
+
+static int	take_a_fork(int index, t_philo *philo)
+{
+	t_timestamp	now_ms;
+
+	while (42)
+	{
+		pthread_mutex_lock(&philo->rule->system_status_mutex);
+		if (philo->rule->is_finished)
+		{
+			pthread_mutex_unlock(&philo->rule->system_status_mutex);
+			return (1);
+		}
+		pthread_mutex_lock(&philo->fork_mutex);
+		if (philo->exist_my_fork)
+		{
+			philo->exist_my_fork = false;
+			now_ms = get_timestamp_us() / 1000;
+			printf("%lld %d has taken a fork\n", now_ms, index);
+			pthread_mutex_unlock(&philo->fork_mutex);
+			pthread_mutex_unlock(&philo->rule->system_status_mutex);
+			return (0);
+		}
+		pthread_mutex_unlock(&philo->fork_mutex);
+		pthread_mutex_unlock(&philo->rule->system_status_mutex);
+		usleep(100);
+	}
 }
 
 static int	return_the_fork(t_philo *philo)
