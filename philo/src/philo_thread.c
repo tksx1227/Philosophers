dc15faf35009e0c7575085691e9d48952afbb932
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2022-07-03 23:31:23 +0900

    ⚡ 偶数番目のスレッドを先に生成する

--- philo/src/philo_thread.c
+++ philo/src/philo_thread.c
@@ -6,30 +6,22 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 09:29:42 by ttomori           #+#    #+#             */
-/*   Updated: 2022/07/03 15:36:29 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/07/03 23:19:02 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+static int	create_philo_threads_half(t_philo *head, int start_index);
 static void	*main_routine(void *content);
 
 int	create_philo_threads(t_philo *head)
 {
-	int		i;
-	int		n_of_philos;
-	t_philo	*philo;
-
-	i = 0;
-	philo = head;
-	n_of_philos = head->info->n_of_philos;
-	while (i < n_of_philos)
-	{
-		if (pthread_create(&philo->thread, NULL, &main_routine, philo))
-			return (1);
-		philo = philo->next;
-		i++;
-	}
+	if (create_philo_threads_half(head, 0))
+		return (1);
+	usleep(50);
+	if (create_philo_threads_half(head->next, 1))
+		return (1);
 	return (0);
 }
 
@@ -71,13 +63,30 @@ int	destroy_all_mutex(t_philo *head)
 	return (0);
 }
 
+static int	create_philo_threads_half(t_philo *head, int start_index)
+{
+	long	i;
+	long	n_of_philos;
+	t_philo	*philo;
+
+	philo = head;
+	i = start_index;
+	n_of_philos = head->info->n_of_philos;
+	while (i < n_of_philos)
+	{
+		if (pthread_create(&philo->thread, NULL, &main_routine, philo))
+			return (1);
+		philo = philo->next->next;
+		i += 2;
+	}
+	return (0);
+}
+
 static void	*main_routine(void *content)
 {
 	t_philo	*philo;
 
 	philo = (t_philo *)content;
-	if (philo->index % 2 == 0)
-		usleep(300);
 	while (42)
 	{
 		if (take_two_forks(philo) \
